8a6f87bb16f1e47226dd2ead61ce931db72268bc
einarpe <einarpe@github.com>
2018-06-21 20:25:10 +0200

    nowe pliki sql

--- SQLSDProject/DimDate.sql
+++ SQLSDProject/DimDate.sql
@@ -10,5 +10,15 @@
     [Minute] TINYINT NOT NULL, 
     [Second] TINYINT NOT NULL, 
     [ShortDateString] CHAR(10) NOT NULL, 
-    [LongDateString] CHAR(19) NOT NULL
+    [LongDateString] CHAR(19) NOT NULL,
+	[DayOfWeekString] as 
+		(case [DayOfWeek]
+			when 1 then 'Monday'
+			when 2 then 'Tuesday'
+			when 3 then 'Wednesday'
+			when 4 then 'Thursday'
+			when 5 then 'Friday'
+			when 6 then 'Saturday'
+			when 7 then 'Sunday'
+		end) 
 )
