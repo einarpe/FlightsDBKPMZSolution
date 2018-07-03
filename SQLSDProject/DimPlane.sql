3ec9765e503a7e7dfbe26dd73169664efb8bccdd
einarpe <einarpe@github.com>
2018-07-03 19:59:04 +0200

    w koncu działa

--- SQLSDProject/DimPlane.sql
+++ SQLSDProject/DimPlane.sql
@@ -2,10 +2,10 @@
 (
 	/* tailnum,type,manufacturer,issue_date,model,status,aircraft_type,engine_type,year */
 	[PlaneKey] INT NOT NULL identity(1,1) PRIMARY KEY,
-	[TailNum] varchar(6) not null UNIQUE,
+	[TailNum] varchar(16) not null UNIQUE,
 	[Type] varchar(64),
 	[Manufacturer] varchar(32),
-	[IssueDate] datetime2,
+	[IssueDate] date,
 	[Model] varchar(32),
 	[Status] varchar(32),
 	[AircraftType] varchar(64),
